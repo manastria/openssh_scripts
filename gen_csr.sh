fd682911b2542cb3d02f54289031834dc1730255
jpdemory <jpdemory@manastria.fr>
2023-10-03 10:01:20 +0200

    V0.2
    Simplification des scripts

--- gen_csr.sh
+++ gen_csr.sh
@@ -6,26 +6,35 @@ usage() {
   echo "Ce script génère une demande de signature de certificat (CSR) et la clé privée associée."
   echo "Les paramètres du certificat peuvent être définis dans le fichier de configuration 'cert_config.cfg'."
   echo ""
-  echo "Usage: $0 <nom_du_fichier_csr>"
+  echo "Usage: $0 <common_name> [san]"
   echo ""
   echo "Arguments :"
-  echo "  <nom_du_fichier_csr>    Le nom du fichier CSR."
+  echo "  <common_name>          Le nom du domaine pour le certificat."
+  echo "  [san]                  Les Subject Alternative Names (optionnel)."
   echo ""
   echo "Exemple :"
-  echo "  $0 my_csr"
+  echo "  $0 www.example.com \"DNS:example.com,IP:192.168.0.1\""
   exit 1
 }
 
-# Vérifie si un argument a été fourni pour le nom du fichier CSR
-if [ "$#" -ne 1 ]; then
+# Vérifie si le premier argument a été fourni
+if [ "$#" -lt 1 ]; then
     usage
 fi
 
 # Charger le fichier de configuration s'il existe
 [ -f "./cert_config.cfg" ] && source "./cert_config.cfg"
 
-# Nom du fichier CSR
-CSR_NAME="$1"
+# Le nom commun (CN) est le nom de domaine du certificat donc du serveur
+COMMON_NAME="$1"
+
+# Si SAN est fourni en tant qu'argument, utilisez-le. Sinon, utilisez COMMON_NAME.
+# Exemple de syntaxe : SAN="${SAN:-DNS:$COMMON_NAME,DNS:example.com,IP:192.168.0.1}"
+if [ "$#" -ge 2 ]; then
+    SAN="$2"
+else
+    SAN="DNS:$COMMON_NAME"
+fi
 
 # Paramètres pour le CSR (ces valeurs écraseront les valeurs du fichier de configuration s'il y en a)
 COUNTRY="${COUNTRY:-FR}"
@@ -33,14 +42,10 @@ STATE="${STATE:-Ile-de-France}"
 LOCALITY="${LOCALITY:-Paris}"
 ORGANIZATION="${ORGANIZATION:-MonOrganisation}"
 ORG_UNIT="${ORG_UNIT:-IT}"
-COMMON_NAME="${COMMON_NAME:-www.example.com}"
 EMAIL="${EMAIL:-admin@example.com}"
 
-# Définition des SAN (Subject Alternative Names)
-SAN="${SAN:-DNS:$COMMON_NAME,DNS:example.com,IP:192.168.0.1}"
-
 # Création du fichier de configuration temporaire pour openssl
-cat > ${CSR_NAME}_openssl.cnf <<EOL
+cat > ${COMMON_NAME}_openssl.cnf <<EOL
 [req]
 distinguished_name = req_distinguished_name
 req_extensions = v3_req
@@ -74,18 +79,17 @@ subjectAltName = $SAN
 EOL
 
 # Génération de la clé privée
-openssl genpkey -algorithm RSA -out "${CSR_NAME}_key.pem"
+openssl genpkey -algorithm RSA -out "${COMMON_NAME}_key.pem"
 if [ $? -ne 0 ]; then
     echo "Erreur lors de la génération de la clé privée."
     exit 1
 fi
 
-
 # Génération du CSR
 openssl req -new \
-    -key "${CSR_NAME}_key.pem" \
-    -out "${CSR_NAME}.csr" \
-    -config "${CSR_NAME}_openssl.cnf" \
+    -key "${COMMON_NAME}_key.pem" \
+    -out "${COMMON_NAME}.csr" \
+    -config "${COMMON_NAME}_openssl.cnf" \
     -batch
 if [ $? -ne 0 ]; then
     echo "Erreur lors de la génération du CSR."
@@ -93,10 +97,11 @@ if [ $? -ne 0 ]; then
 fi
 
 # Nettoyage du fichier de configuration temporaire
-rm ${CSR_NAME}_openssl.cnf
+rm ${COMMON_NAME}_openssl.cnf
 if [ $? -ne 0 ]; then
     echo "Erreur lors de la suppression du fichier de configuration temporaire."
     exit 1
 fi
 
 echo "CSR et clé privée générées avec succès."
+
