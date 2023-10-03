36e37d2436455c572c0e67a43a4a0abf60b04630
jpdemory <jpdemory@manastria.fr>
2023-10-03 09:32:32 +0200

    Amélioration de script sign_csr.crt

--- sign_csr.sh
+++ sign_csr.sh
@@ -6,67 +6,80 @@ usage() {
     echo "  Ce script permet de signer un Certificate Signing Request (CSR) avec une autorité de certification (CA) spécifiée."
     echo "  Si aucune CA n'est fournie, un certificat auto-signé sera généré."
     echo ""
-    echo "Usage: $0 <nom_fichier_csr> [nom_fichier_ca]"
+    echo "Usage: $0 <common_name> [nom_fichier_ca]"
     echo "Arguments :"
-    echo "  <nom_fichier_csr>       : Le nom du fichier CSR à signer."
-    echo "  [nom_fichier_ca]        : (Optionnel) Le nom de la CA utilisée pour signer le CSR."
-    echo "                            Si omis, un certificat auto-signé sera créé."
+    echo "  <common_name>       : Le nom du fichier CSR à signer."
+    echo "  [nom_ca]            : (Optionnel) Le nom de la CA utilisée pour signer le CSR."
+    echo "                        Si omis, un certificat auto-signé sera créé."
 }
 
-# Affichage de l'aide si aucun argument n'est fourni
-if [ "$#" -lt 1 ]; then
-    usage
-    exit 1
-fi
+check_dependencies() {
+    if ! command -v openssl &> /dev/null; then
+        echo "Erreur : openssl n'est pas installé."
+        exit 1
+    fi
+}
 
-# Charger le fichier de configuration s'il existe
-[ -f "./cert_config.cfg" ] && source "./cert_config.cfg"
+extract_common_name() {
+    COMMON_NAME=$(openssl req -in "${CSR_NAME}.csr" -noout -subject | sed -n 's/.*CN *= *\([^ ,]*\).*/\1/p')
+    echo "Common Name : ${COMMON_NAME}"
+}
 
-# Nom du fichier CSR et éventuelle CA
-CSR_NAME="$1"
-CA_NAME="$2"
-
-# Extraction du Common Name du CSR
-COMMON_NAME=$(openssl req -in "${CSR_NAME}.csr" -noout -subject | sed -n 's/.*CN[[:space:]]*=[[:space:]]*\([a-zA-Z0-9\.\-]*\).*/\1/p')
-
-# Afficher le Common Name du CSR
-echo "Common Name : ${COMMON_NAME}"
-
-# Vérifier que le Common Name du CSR est valide
-if [ -z "$COMMON_NAME" ]; then
-    echo "Erreur : Le Common Name du CSR ne peut pas être vide."
-    exit 1
-fi
-
-# Si une CA est fournie
-if [ ! -z "$CA_NAME" ]; then
+sign_with_ca() {
     openssl x509 -req \
         -in "${CSR_NAME}.csr" \
         -CA "${CA_NAME}_ca_cert.pem" \
         -CAkey "${CA_NAME}_ca_key.pem" \
         -CAcreateserial \
         -out "${COMMON_NAME}_cert.pem"
-
-    # Création de la chaîne de certificats
+    
     cat "${COMMON_NAME}_cert.pem" "${CA_NAME}_ca_cert.pem" > "${COMMON_NAME}_chain.pem"
+}
 
-# Si aucune CA n'est fournie, création d'un certificat auto-signé
-else
+create_self_signed() {
     openssl x509 -req \
         -in "${CSR_NAME}.csr" \
         -signkey "${CSR_NAME}_key.pem" \
         -out "${COMMON_NAME}_cert.pem"
-
-    # Dans ce cas, la chaîne de certificat est simplement le certificat auto-signé
+    
     cp "${COMMON_NAME}_cert.pem" "${COMMON_NAME}_chain.pem"
-fi
+}
 
-# Export en format PKCS12
-openssl pkcs12 -export \
-    -inkey "${CSR_NAME}_key.pem" \
-    -in "${COMMON_NAME}_cert.pem" \
-    -certfile "${COMMON_NAME}_chain.pem" \
-    -out "${COMMON_NAME}.p12" \
-    -passout pass:
+export_pkcs12() {
+    openssl pkcs12 -export \
+        -inkey "${CSR_NAME}_key.pem" \
+        -in "${COMMON_NAME}_cert.pem" \
+        -certfile "${COMMON_NAME}_chain.pem" \
+        -out "${COMMON_NAME}.p12" \
+        -passout pass:
+}
 
-echo "Certificat généré avec succès."
+main() {
+    if [[ "$#" -lt 1 ]]; then
+        usage
+        exit 1
+    fi
+
+    [ -f "./cert_config.cfg" ] && source "./cert_config.cfg"
+    CSR_NAME="$1"
+    CA_NAME="$2"
+
+    check_dependencies
+    extract_common_name
+    
+    if [[ -z "${COMMON_NAME}" ]]; then
+        echo "Erreur : Le Common Name du CSR ne peut pas être vide."
+        exit 1
+    fi
+    
+    if [[ -n "${CA_NAME}" ]]; then
+        sign_with_ca
+    else
+        create_self_signed
+    fi
+    
+    export_pkcs12
+    echo "Certificat généré avec succès."
+}
+
+main "$@"
